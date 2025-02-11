0e718248f5bcde527e579efb3596c27f5c585935
PilaFFF <vsevolod_derzhavin2@mail.ru>
2025-02-11 13:02:55 +0300

    fix task page h1 | change theme btn

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import './App.css';
+import './App.scss';
 import { BrowserRouter } from 'react-router-dom';
 import AppRoutes from './routes/AppRoutes';
 import { useEffect, useState } from 'react';
